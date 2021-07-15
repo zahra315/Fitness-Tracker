b92736205a81bffb945d16c7072ebab2eccbb7d6
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-14 23:30:36 -0700

    define properties and types

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({});
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: { type: String, require: true },
+      name: { type: String, require: true },
+      duration: { type: Number, require: true },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number },
+      distance: { type: Number },
+    },
+  ],
+});
 
 const Workout = mongoose.model("workout", workoutSchema);
 module.exports = Workout;
