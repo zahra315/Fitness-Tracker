d85f5b9f35f45579d1604c85c81f3d019c2d5245
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-16 13:50:21 -0700

    api routing

---
+++ routes/workoutRoutes.js
@@ -0,0 +1,53 @@
+const Workout = require("../models/workout");
+
+module.exports = (app) => {
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then((workoutDB) => {
+        // console.log(workoutDB);
+        res.json(workoutDB);
+      })
+      .catch((error) => {
+        res.json(error);
+      });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $inc: { totalDuration: req.body.duration },
+        $push: { exercises: req.body },
+      },
+      { new: true, runValidators: true }
+    )
+      .then((workoutDB) => {
+        res.json(workoutDB);
+      })
+      .catch((error) => {
+        res.json(error);
+      });
+  });
+
+  app.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+      .then((workoutDB) => {
+        res.json(workoutDB);
+      })
+      .catch((error) => {
+        res.json(error);
+      });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then((workoutDB) => {
+        // console.log(workoutDB);
+
+        res.json(workoutDB);
+      })
+      .catch((error) => {
+        res.json(error);
+      });
+  });
+};
