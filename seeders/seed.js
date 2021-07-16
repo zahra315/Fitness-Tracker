dd08ffc8b3b5bd4823950b36df50329b0cb59d17
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-15 23:55:08 -0700

    seeds the DB

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,19 +1,25 @@
-const mongoose = require('mongoose');
-const db = require('../models');
+require("dotenv").config;
 
-mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+const mongoose = require("mongoose");
+const db = require("../models/workout");
+const connect = require("../config/db");
+const connectDB = require("../config/db");
+
+connectDB();
+
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
 
 const workoutSeed = [
   {
     day: new Date(new Date().setDate(new Date().getDate() - 9)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Bicep Curl',
+        type: "resistance",
+        name: "Bicep Curl",
         duration: 20,
         weight: 100,
         reps: 10,
@@ -25,8 +31,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 8)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Lateral Pull',
+        type: "resistance",
+        name: "Lateral Pull",
         duration: 20,
         weight: 300,
         reps: 10,
@@ -38,8 +44,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 7)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Push Press',
+        type: "resistance",
+        name: "Push Press",
         duration: 25,
         weight: 185,
         reps: 8,
@@ -51,8 +57,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 6)),
     exercises: [
       {
-        type: 'cardio',
-        name: 'Running',
+        type: "cardio",
+        name: "Running",
         duration: 25,
         distance: 4,
       },
@@ -62,8 +68,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 5)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Bench Press',
+        type: "resistance",
+        name: "Bench Press",
         duration: 20,
         weight: 285,
         reps: 10,
@@ -75,8 +81,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 4)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Bench Press',
+        type: "resistance",
+        name: "Bench Press",
         duration: 20,
         weight: 300,
         reps: 10,
@@ -88,8 +94,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 3)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Quad Press',
+        type: "resistance",
+        name: "Quad Press",
         duration: 30,
         weight: 300,
         reps: 10,
@@ -101,8 +107,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 2)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Bench Press',
+        type: "resistance",
+        name: "Bench Press",
         duration: 20,
         weight: 300,
         reps: 10,
@@ -114,8 +120,8 @@ const workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 1)),
     exercises: [
       {
-        type: 'resistance',
-        name: 'Military Press',
+        type: "resistance",
+        name: "Military Press",
         duration: 20,
         weight: 300,
         reps: 10,
@@ -125,13 +131,27 @@ const workoutSeed = [
   },
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
-  .then((data) => {
-    console.log(data.result.n + ' records inserted!');
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
+// db.Workout.deleteMany({})
+//   .then(() => db.Workout.collection.insertMany(workoutSeed))
+//   .then((data) => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     process.exit(1);
+//   });
+
+const importDta = async () => {
+  try {
+    await db.deleteMany({});
+    await db.insertMany(workoutSeed);
+    console.log("Data Import Success");
+    process.exit();
+  } catch (error) {
+    console.error("Error with data import");
     process.exit(1);
-  });
+  }
+};
+
+importDta();
