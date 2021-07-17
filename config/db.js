091c88621e54a6c0ea04a9d59a568395cfef35a7
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-16 23:10:14 -0700

    fix deprecation warnings

--- config/db.js
+++ config/db.js
@@ -6,6 +6,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
     });
 
     console.log("MongoDB connection SUCCESS");
