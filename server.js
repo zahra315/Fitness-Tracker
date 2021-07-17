7116c2c6b005cf431261e6a58f061709156e673d
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-16 23:15:55 -0700

    require routes

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-const workoutRoutes = require("./routes/workoutRoutes");
-
-// connectDB();
 
 const app = express();
 
-app.use(express.json());
-
-// app.use("/api/workouts", workoutRoutes);
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+connectDB();
+
+require("./routes/app")(app);
+require("./routes/workoutRoutes")(app);
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
