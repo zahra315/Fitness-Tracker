e8cba36941bbfe6443ddca9c1d39787d0bf7367a
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-18 00:39:54 -0700

    typerroe

--- routes/app.js
+++ routes/app.js
@@ -1,14 +1,14 @@
 const path = require("path");
 module.exports = (app) => {
   app.get("/", (req, res) => {
-    res.senFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/exercise", (re, res) => {
-    res.senFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
-  app.get("/state", (re, res) => {
-    res.senFile(path.join(__dirname, "../public/stats.html"));
+  app.get("/stats", (re, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 };
