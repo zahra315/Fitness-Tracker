01e85c970cafd6692f9401e4ab6e0c8763c150fe
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-21 18:00:41 -0700

    debugging

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://localhost/deep-thoughts",
+      process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
